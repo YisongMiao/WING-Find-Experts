8e6d4c0c29787804340ea8811c311c87042e72fa
Ruiwen Zhou <skyriver@sjtu.edu.cn>
2025-04-17 12:58:55 +0800

    [fix] bugs: embedding and arguments. [modify] avoid parsing URLs every run.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import json
 import argparse
 import numpy as np
@@ -20,9 +21,10 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--data_path", type=str, default="./data/test_database.jsonl", help="Path to the database file")
     parser.add_argument("--query_path", type=str, default="./data/test_query.json", help="Path to the query file")
+    parser.add_argument("--output_path", type=str, default="./log/output.txt", help="Path to the output file")
     parser.add_argument("--llm", type=str, default="qwen2.5-72b-instruct", help="LLM to generate justification of fitness score.")
     parser.add_argument("--embedding_model_name_or_path", type=str, default="all-MiniLM-L6-v2", help="Embedding model name or path.")
-    parser.add_argument("--author_embedding", type=str, choices=["aggregate", "summarize"], default="aggregate", help="Method to aggregate multiple publications for one author.")
+    parser.add_argument("--author_embedding", type=str, choices=["aggregate", "summarize"], default="summarize", help="Method to aggregate multiple publications for one author.")
     parser.add_argument("--device", type=str, default="cpu", help="Computation device to store the embedding model and compute embeddings.")
     args = parser.parse_args()
     return args
@@ -79,18 +81,18 @@ def compute_fitness(model: SentenceTransformer, query: str, list_of_authors: lis
     Returns:
         search_dict (list[dict[str, Union[int, float]]]): A list of dict with the keys 'corpus_id' and 'score', sorted by decreasing cosine similarity scores.
     """
-    query_embedding = model.encode(query, normalize_embeddings=True).unsqueeze(0)
+    query_embedding = model.encode(query, normalize_embeddings=True, convert_to_tensor=True).unsqueeze(0).to(args.device)
     if args.author_embedding == "aggregate":
         # Compute the range of publications belonging to each author
         n_pub_per_author = [len(pubs) for pubs in list_of_authors]
         low, high = np.cumsum([0] + n_pub_per_author[:-1]), np.cumsum(n_pub_per_author)
         # Stack all publications together and compute embedding only once
         stacked_pubs = sum(list_of_authors, [])
-        all_pub_embedding = model.encode_multi_process(stacked_pubs, normalize_embeddings=True)
+        all_pub_embedding = model.encode(stacked_pubs, normalize_embeddings=True, convert_to_tensor=True).to(args.device)
         # Seperately aggregate the embedding of each author
         corpus_embedding = torch.stack([torch.mean(all_pub_embedding[l:h]) for l, h in zip(low, high)], dim=0)
     elif args.author_embedding == "summarize":
-        corpus_embedding = model.encode_multi_process(list_of_authors, normalize_embeddings=True)
+        corpus_embedding = model.encode(list_of_authors, normalize_embeddings=True, convert_to_tensor=True).to(args.device)
     else:
         raise NotImplementedError(f"Author Embedding:{args.author_embedding} method not implemented")
     return semantic_search(query_embedding, corpus_embedding, top_k=len(list_of_authors))[0]
@@ -102,11 +104,20 @@ def main():
     model = init_model(args.embedding_model_name_or_path, args.device)
     
     # Input query and build database
-    query_dict = json.load(args.query_path)
-    database = read_jsonl(args.data_path)
+    with open(args.query_path, "r") as f:
+        query_dict = json.load(f)
     
-    # Process the database so that each item provides more details of each author
-    database = build_author_profile(database, args)
+    if not os.path.exists("log/author_profile.json"):
+        database = read_jsonl(args.data_path)
+        
+        # Process the database so that each item provides more details of each author
+        database = build_author_profile(database, args)
+        with open("log/author_profile.json", "w") as f:
+            json.dump(database, f)
+    
+    else:
+        with open("log/author_profile.json", "r") as f:
+            database = json.load(f)
     
     # Process the query so that the query can be more specific
     query = build_query(query_dict)
@@ -119,8 +130,10 @@ def main():
     results = []
     for item in tqdm(sorted_id_and_scores):
         author_id, score = item["corpus_id"], item["score"]
-        reason = generate_justification(query_dict, database[author_id]["summary"], round(score * 100))
+        author_name = database[author_id]["name"]
+        reason = generate_justification(query_dict, database[author_id]["summary"], round(score * 100), args)
         results.append({
+            "name": author_name,
             "fitness": score,
             "explanation": reason
         })
